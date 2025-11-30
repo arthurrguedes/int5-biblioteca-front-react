8ce12d5ba5cb23cdf00f84dd4e2aecd964ebf19b
ADFS2050 <agcyborgdygital@gmail.com>
2025-11-30 05:03:25 -0300

    feat: Implementa mapeamento de capas por ISBN com dados hardcoded

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,16 +1,34 @@
-import React from 'react';
+// src/components/BookCard/BookCard.js
+
+import React from 'react'; 
 import { Link } from 'react-router-dom';
 import { FaBookOpen } from 'react-icons/fa';
 import styles from './BookCard.module.css';
+// ALTERE A LINHA ABAIXO PARA INCLUIR O '.js'
+import { CUSTOM_COVER_MAP } from '../../mapeamento/coverMap.js'; // <-- CORRIGIDO AQUI!
 
 const BookCard = ({ book, linkTo }) => {
   const Wrapper = linkTo ? Link : 'div';
+  
+  // Tenta buscar a URL no mapa usando o ISBN (inventado) como chave
+  const coverUrl = CUSTOM_COVER_MAP[book.isbn]; 
 
   return (
     <Wrapper to={linkTo} className={styles.card}>
-      <div className={styles.placeholder}>
-        <FaBookOpen />
-      </div>
+      {coverUrl ? (
+          // Exibe a imagem se a URL foi encontrada no mapa
+          <img 
+            src={coverUrl} 
+            alt={`Capa do livro ${book.title}`} 
+            className={styles.coverImage} 
+          />
+      ) : (
+          // Se o ISBN não estiver mapeado, exibe o placeholder.
+          <div className={styles.placeholder}>
+            <FaBookOpen />
+          </div>
+      )}
+      
       <div>
         <div className={styles.title} title={book.title}>{book.title}</div>
         <div className={styles.author}>{book.author}</div>
---
+++ src/mapeamento/coverMap.js
@@ -0,0 +1,59 @@
+// src/components/BookCard/coverMap.js
+
+export const CUSTOM_COVER_MAP = {
+    // Mapeamento sequencial dos 26 ISBNs para as 22 URLs fornecidas.
+
+    //Capas da home
+    "9788576572001": "https://ocapista.com.br/imgs/capas/a_arvore_da_vida.jpg",
+    "9780141439518": "https://ocapista.com.br/imgs/capas/luta_diaria.jpg",
+    "9780062693662": "https://ocapista.com.br/imgs/capas/sobre_fins_e_inicios.jpg",
+    "9780618260300": "https://ocapista.com.br/imgs/capas/capa_livro_100d.jpg",
+    "9788599296318": "https://ocapista.com.br/imgs/capas/capa_livro_100a.jpg",
+    "9788599296123": "https://ocapista.com.br/imgs/capas/capa_livro_100b.jpg",
+    "9780618640157": "https://ocapista.com.br/imgs/capas/capa_livro_100e.jpg",
+    "8640157978061": "https://ocapista.com.br/imgs/capas/capa_livro_100e.jpg",
+    "6408061815797": "https://ocapista.com.br/imgs/capas/a_alienator.jpg",
+    "6584513629": "https://ocapista.com.br/imgs/capas/a_capa_ruins_of_the_empire.jpg",
+    "8575225634": "https://ocapista.com.br/imgs/capas/a_historias_da_ciencia_oriental.jpg",
+    "7723033369024" : "https://ocapista.com.br/imgs/capas/a_cura_pelas_pedras.jpg",
+    "9793539927343" : "https://ocapista.com.br/imgs/capas/a_curta_mais_a_sua_vida.jpg",
+    "5520951748932" : "https://ocapista.com.br/imgs/capas/a_sundered_moon.jpg",
+    "5286266689728" : "https://ocapista.com.br/imgs/capas/a_doses_de_sabedora.jpg",
+    "6939914769776" : "https://ocapista.com.br/imgs/capas/a_sundered_moon.jpg",
+    "5073778549235" : "https://ocapista.com.br/imgs/capas/a_sundered_moon.jpg",
+
+
+
+
+
+
+
+
+    // 1 a 22: Mapeamento direto
+    "1418030289687": "https://ocapista.com.br/imgs/capas/livro_de_horror_capa_livro_2.jpg", 
+    "1456106348938": "https://ocapista.com.br/imgs/capas/capa_livro_fantasia_romance.jpg",
+    "1550461768468": "https://ocapista.com.br/imgs/capas/fantasia_capa_livro.jpg",
+    "1619023273534": "https://ocapista.com.br/imgs/capas/capa_livro_100c.jpg",
+    "1994123428622": "https://ocapista.com.br/imgs/capas/a_filosofia_de_branca_de_neve_alice_no_pais_das_maravilhas_e_outras_historias_infantis.jpg",
+    "2376485145387": "https://ocapista.com.br/imgs/capas/capa_livro_corvos.jpg",
+    "2377675566459": "https://ocapista.com.br/imgs/capas/capa_livro_100f.jpg",
+    "2385749459884": "https://ocapista.com.br/imgs/capas/embebicao.jpg",
+    "2454597516203": "https://ocapista.com.br/imgs/capas/a_arte_da_guerra_capa_livro.jpg",
+    "2558322421335": "https://ocapista.com.br/imgs/capas/1984_capa_livro_george_orwell.jpg", // URL 10
+    "2763642454007": "https://ocapista.com.br/imgs/capas/1984_capa_livro_george_orwell.jpg", // URL 11 (Repetição na sua lista)
+    "2767538402234": "https://ocapista.com.br/imgs/capas/livro_de_suspense_noir_livro_capa.jpg",
+    "3005706953971": "https://ocapista.com.br/imgs/capas/caminhos_da_escuridao.jpg",
+    "3280948294539": "https://ocapista.com.br/imgs/capas/alem_do_hype.jpg",
+    "3349423242185": "https://ocapista.com.br/imgs/capas/a_noirte_marcada.jpg",
+    "3458979365346": "https://ocapista.com.br/imgs/capas/duras_estradas.jpg",
+    "3535846712762": "https://ocapista.com.br/imgs/capas/inteligencia_e.jpg",
+    "3737815362608": "https://ocapista.com.br/imgs/capas/chips_do_amanha.jpg",
+    "3840059391826": "https://ocapista.com.br/imgs/capas/capas_o_general_frances.jpg",
+    "4033144732811": "https://ocapista.com.br/imgs/capas/desamores.jpg",
+    "4120327448157": "https://ocapista.com.br/imgs/capas/bruta.jpg",
+    "43566695349458": "https://ocapista.com.br/imgs/capas/capa_livro_fantasia.jpg", // URL 22
+    "4600160224779": "https://ocapista.com.br/imgs/capas/guardei_as_pedras.jpg", // Repete URL 1
+    "4701733351764": "https://ocapista.com.br/imgs/capas/revolucao_dos_bichos.jpg", // Repete URL 2
+    "4729589486901": "https://ocapista.com.br/imgs/capas/saude_mental.jpg", // Repete URL 3
+    "4876641795863": "https://ocapista.com.br/imgs/capas/a_arte_do_incompleto.jpg" // Repete URL 4
+};
